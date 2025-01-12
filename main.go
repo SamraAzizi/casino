e2015495556e4ac775746f1a9c540a05c7b7183c
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-01-12 18:43:21 +0500

    casino-spin

--- main.go
+++ main.go
@@ -6,40 +6,6 @@ import (
 	"time"
 )
 
-// Function to get the user's name
-func getName() string {
-	var name string
-
-	fmt.Println("Welcome to Samra's Casino...")
-	fmt.Printf("Enter your name: ")
-	_, err := fmt.Scanln(&name)
-	if err != nil {
-		fmt.Println("Error reading input. Please try again.")
-		return ""
-	}
-	fmt.Printf("Welcome %s, let's play!\n", name)
-	return name
-}
-
-// Function to get the user's bet
-func getBet(balance uint) uint {
-	var bet uint
-	for {
-		fmt.Printf("Enter your bet, or 0 to quit (balance = $%d): ", balance)
-		_, err := fmt.Scan(&bet)
-		if err != nil {
-			fmt.Println("Invalid input. Please enter a valid number.")
-			continue
-		}
-		if bet > balance {
-			fmt.Println("Bet cannot be larger than balance.")
-		} else {
-			break
-		}
-	}
-	return bet
-}
-
 // Function to generate a symbol array from a symbol-to-count map
 func generateSymbolArray(symbols map[string]uint) []string {
 	symbolArr := []string{}
@@ -133,10 +99,10 @@ func main() {
 
 	symbolArr := generateSymbolArray(symbols)
 	balance := uint(200)
-	getName()
+	GetName()
 
 	for balance > 0 {
-		bet := getBet(balance)
+		bet := GetBet(balance)
 		if bet == 0 {
 			break
 		}
